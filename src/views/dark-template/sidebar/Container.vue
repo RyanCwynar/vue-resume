f8b2a9792557e43ffdc7d0d85ab9bc2135eeacd3
Ryan Cwynar <ryan@fueledonbacon.com>
2019-10-14 16:57:44 -0400

    remove unused sections

--- src/views/dark-template/sidebar/Container.vue
+++ src/views/dark-template/sidebar/Container.vue
@@ -16,50 +16,6 @@
 
       <sidebar-section :options="sections.info" />
       <sidebar-section :options="sections.socials" />
-      <sidebar-section :options="sections.hobbies">
-        <template v-slot:item="{item}">
-          <v-chip>
-            <v-avatar>
-              <v-icon>
-                {{ item.icon }}
-              </v-icon>
-            </v-avatar>
-            {{ item.text }}
-          </v-chip>
-        </template>
-      </sidebar-section>
-      <sidebar-section :options="sections.languages">
-        <template v-slot:items="{items}">
-          <v-container pa-0>
-            <v-layout
-              wrap
-              class="text-xs-center"
-            >
-              <template
-                v-for="(item, i) in items"
-              >
-                <v-flex
-                  :key="i"
-                  md3
-                  sm4
-                  xs6
-                >
-                  <v-progress-circular
-                    rotate="360"
-                    size="65"
-                    width="2"
-                    :value="item.value"
-                    color="white"
-                    class="ma-2"
-                  >
-                    {{ item.text }}
-                  </v-progress-circular>
-                </v-flex>
-              </template>
-            </v-layout>
-          </v-container>
-        </template>
-      </sidebar-section>
     </v-card-text>
   </v-card>
 </template>
